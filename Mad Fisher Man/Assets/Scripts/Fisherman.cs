8839ee597340a11e9a9b77a80eaa8d4a6286557f
Ayberk Ortancalar <ortancalarayberk@gmail.com>
2021-05-29 18:18:38 +0300

    SellBabySell

--- Mad Fisher Man/Assets/Scripts/Fisherman.cs
+++ Mad Fisher Man/Assets/Scripts/Fisherman.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEditorInternal.Profiling.Memory.Experimental;
 using UnityEngine;
+using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
 using Random = UnityEngine.Random;
 
 public class Fisherman : MonoBehaviour
@@ -11,6 +14,9 @@ public class Fisherman : MonoBehaviour
     [SerializeField] private UI_Inventory uiInventory;
     private Inventory inventory;
     private int emptySlot = 8;
+    private int luckyNumber;
+    private int purse;
+    public Text coin;
 
     private void Awake()
     {
@@ -18,13 +24,16 @@ public class Fisherman : MonoBehaviour
         inventory = new Inventory();
         uiInventory.SetInventory(inventory);
         uiInventory.SetPlayer(this);
-        
+        purse = 0;
+
     }
     public Vector3 GetPosition() {
         return transform.position;
     }
-    private int luckyNumber;
     
+
+
+   
     void Update()
     {
         if (emptySlot !=8 )
@@ -75,7 +84,57 @@ public class Fisherman : MonoBehaviour
                 Debug.Log(inventory.GetItemList().Count);
             }
         }
-        
-        
+    }
+    public void sellButton(int price)
+    {
+        Item fish1 = new Item { itemType = Item.ItemType.Fish1};
+        Item fish2 = new Item { itemType = Item.ItemType.Fish2};
+        Item fish3 = new Item { itemType = Item.ItemType.Fish3};
+        Item fish4 = new Item { itemType = Item.ItemType.Fish4};
+        Item fish5 = new Item { itemType = Item.ItemType.Fish5};
+        Item fish6 = new Item { itemType = Item.ItemType.Fish6};
+        Item fish7 = new Item { itemType = Item.ItemType.Fish7};
+        Item fish8 = new Item { itemType = Item.ItemType.Fish8};
+        switch (price)
+        {
+            case 10:
+                removeItem(fish1, 10);
+                break;
+            case 15:
+                removeItem(fish2, 15);
+                break;
+            case 25:
+                removeItem(fish3, 25);
+                break;
+            case 35:
+                removeItem(fish4, 35);
+                break;
+            case 45:
+                removeItem(fish5, 45);
+                break;
+            case 60:
+                removeItem(fish6, 60);
+                break;
+            case 75:
+                removeItem(fish7, 75);
+                break;
+            case 100:
+                removeItem(fish8, 100);
+                break;
+        }
+    }
+
+    public void removeItem(Item x, int price)
+    {
+        foreach (var item in inventory.GetItemList())
+        {
+            if (item.itemType == x.itemType)
+            {
+                inventory.GetItemList().Remove(item);
+                emptySlot++;
+                purse += price;
+                coin.text = purse.ToString();
+            }
+        }
     }
 }
