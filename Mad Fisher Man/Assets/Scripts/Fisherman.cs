7540d45e6d3b14827c6c08abac8c11e6c5e599c3
Ayberk Ortancalar <ortancalarayberk@gmail.com>
2021-06-06 18:06:06 +0300

    UpBabeUp

--- Mad Fisher Man/Assets/Scripts/Fisherman.cs
+++ Mad Fisher Man/Assets/Scripts/Fisherman.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using UnityEditorInternal.Profiling.Memory.Experimental;
 using UnityEngine;
 using UnityEngine.UI;
 using Debug = UnityEngine.Debug;
@@ -20,13 +19,28 @@ public class Fisherman : MonoBehaviour
     private bool fishNow;
     private bool isFishing;
     private bool isThrow;
-
+    
     private float fishTimer;
     private float fishAbleTime;
     private bool isFishingArea;
     public Animator _animator;
     private bool isMissed;
     private bool isCatched;
+    public GameObject fish_icon;
+    private int minTime;
+    private int maxTime;
+    private int currentUpgradeLevel;
+    private int extraLuck;
+
+    private Image olta_image;
+    public Sprite olta1;
+    public Sprite olta2;
+    public Sprite olta3;
+    public Sprite olta4;
+    public Text minText;
+    public Text maxText;
+    public Text extraLuckText;
+    
 
     private void Awake()
     {
@@ -34,25 +48,27 @@ public class Fisherman : MonoBehaviour
         inventory = new Inventory();
         uiInventory.SetInventory(inventory);
         uiInventory.SetPlayer(this);
-        purse = 0;
         fishNow = false;
-        fishTimer = Random.Range(3f, 5f);
+        fishTimer = Random.Range(minTime, maxTime);
         fishAbleTime = 1f;
         isFishing = false;
         isFishingArea = false;
         isThrow = false;
         isMissed = false;
         isCatched = false;
-        
-
+        currentUpgradeLevel = 0;
+        purse = 5000;
+        coin.text = purse.ToString();
+        minTime = 12;
+        maxTime = 16;
+        extraLuck = 0;
+        olta_image = GameObject.Find("Olta_icon").GetComponent<Image>();
+        olta_image.sprite = olta1;
     }
     public Vector3 GetPosition() {
         return transform.position;
     }
     
-
-
-   
     void Update()
     {
         if (isFishingArea && Input.GetKeyDown(KeyCode.K))
@@ -72,7 +88,10 @@ public class Fisherman : MonoBehaviour
             if (fishTimer <= 0)
             {
                 fishNow = true;
+                fish_icon.SetActive(true);
+                
                 Debug.Log("Press Space Now");
+                
             }
             if (fishNow)
             {
@@ -82,11 +101,12 @@ public class Fisherman : MonoBehaviour
             {
                 fishNow = false;
                 isFishing = false;
-                fishTimer = Random.Range(3f, 5f);
+                fishTimer = Random.Range(minTime, maxTime);
                 fishAbleTime = 1f;
                 _animator.SetBool("Throw", false);
                 _animator.SetBool("Fish", false);
                 _animator.SetBool("Catch", true);
+                fish_icon.SetActive(false);
                 if (isMissed)
                 {
                     _animator.SetBool("Missed", true);
@@ -99,8 +119,6 @@ public class Fisherman : MonoBehaviour
                 inventory.GetItemList().RemoveAt(inventory.GetItemList().Count-1);
                 emptySlot++;
             }*/
-
-        
         if (emptySlot !=0 && fishNow)
         {
             isMissed = true;
@@ -113,6 +131,11 @@ public class Fisherman : MonoBehaviour
                 fishNow = false;
                 isCatched = true;
                 luckyNumber = Random.Range(0, 100);
+                luckyNumber += extraLuck;
+                if (luckyNumber > 100)
+                {
+                    luckyNumber = 100;
+                }
                 Debug.Log("You catch a fish!");
                 if (luckyNumber <= 25)
                 {
@@ -149,6 +172,58 @@ public class Fisherman : MonoBehaviour
                 emptySlot--;
                 Debug.Log(inventory.GetItemList().Count);
             }
+        }
+    }
+
+    public void upgradeButton(int x)
+    {
+        switch (x)
+        {
+            case 1:
+                if (purse >=300 && currentUpgradeLevel == 0)
+                {
+                    purse -=300;
+                    coin.text = purse.ToString();
+                    minTime = 10;
+                    maxTime = 14;
+                    extraLuck = 5;
+                    minText.text = minTime.ToString();
+                    maxText.text = maxTime.ToString();
+                    extraLuckText.text = extraLuck.ToString();
+                    olta_image.sprite = olta2;
+                    currentUpgradeLevel++;
+                }
+                break;
+            case 2:
+                if (purse >=500 && currentUpgradeLevel == 1)
+                {
+                    purse -=500;
+                    coin.text = purse.ToString();
+                    minTime = 8;
+                    maxTime = 12;
+                    extraLuck = 10;
+                    minText.text = minTime.ToString();
+                    maxText.text = maxTime.ToString();
+                    extraLuckText.text = extraLuck.ToString();
+                    olta_image.sprite = olta3;
+                    currentUpgradeLevel++;
+                }
+                break;
+            case 3:
+                if (purse >=1000 && currentUpgradeLevel == 2)
+                {
+                    purse -=1000;
+                    coin.text = purse.ToString();
+                    minTime = 6;
+                    maxTime = 10;
+                    extraLuck = 15;
+                    minText.text = minTime.ToString();
+                    maxText.text = maxTime.ToString();
+                    extraLuckText.text = extraLuck.ToString();
+                    olta_image.sprite = olta4;
+                    currentUpgradeLevel++;
+                }
+                break;
         }
     }
     public void sellButton(int price)
@@ -189,7 +264,6 @@ public class Fisherman : MonoBehaviour
                 break;
         }
     }
-
     public void removeItem(Item x, int price)
     {
         foreach (var item in inventory.GetItemList())
@@ -203,7 +277,6 @@ public class Fisherman : MonoBehaviour
             }
         }
     }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("fishArea"))
